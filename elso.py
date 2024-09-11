c0266743dd221f6c8d5cac996550c1b31ddacbca
Zsolti705 <155627430+Zsolti705@users.noreply.github.com>
2024-09-11 12:50:42 +0200

    Add files via upload

---
+++ elso.py
@@ -0,0 +1,22 @@
+"""
+a = 5
+b = 10.5
+m = 3.5
+k = 2*(a+b)
+t = a*b
+V = t*m
+print("A kerület =", k)
+print("A terület =", t)
+print("A térfogat =", V)
+
+Pi = 3.14
+r = 3
+A = 4*Pi*r*r
+print("A felszín =", A)
+V = (4*r**3*Pi)/3
+print(f"A felszín =, {A} \ntérfogata: {V}")
+"""
+o = 4
+A = 6*o*o
+V = o*o*o
+print(f"A felszíne =, {A}\ntérfogata:{V}")
